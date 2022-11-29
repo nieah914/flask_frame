aa4dcfff44e0a32d7af903b90f2e2b24b7df8311
정해인 <nieah914@naver.com>
2022-11-29 15:25:51 +0900

    init

--- model/codeMaster.py
+++ model/codeMaster.py
@@ -1,15 +1,8 @@
 from flask import request
 from flask_restx import Resource, Api, Namespace
 from sql.sql_tools import QueryManager
-import json
 
-CodeMaster = Namespace(name='CodeMaster',
-                       description="코드마스터를 이용한 관리를 위한 부분"
-                       )
-
-
-
-
+CodeMaster = Namespace(name='CodeMaster', description="코드마스터를 이용한 관리를 위한 부분")
 
 @CodeMaster.route('/groupCode')
 class CodeMasterGet(Resource):
@@ -57,97 +50,3 @@ class CodeMasterGet(Resource):
         qm.serviceUpdate()
         results = qm.serviceResults()
         return {}, 200
-@CodeMaster.route('/detailCode')
-class CodeMasterGet(Resource):
-    def get(self):
-        parameter_dict = request.args.to_dict()
-        if len(parameter_dict) == 0:
-            return 'No parameter'
-
-        parameters = ''
-        for key in parameter_dict.keys():
-            parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
-        print(parameters)
-
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'getGroupDetailCode')
-        qm.addInputs(parameter_dict)
-        qm.service()
-        results = qm.serviceResults()
-        if len(results) > 0:
-            print('1.....')
-            return results
-        else :
-            print('2.....')
-            return {},200
-
-    def put(self):
-        parameters = ''
-        if request.method == 'POST' or request.method == 'PUT':
-            parameter_dict = request.form.to_dict()
-        else:
-            parameter_dict = request.args.to_dict()
-            for key in parameter_dict.keys():
-                parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
-            print(parameters)
-        print(parameter_dict)
-
-        print('111111')
-        if len(parameter_dict) == 0:
-            return 'No parameter'
-        print('111122')
-
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'updateDetailCode')
-        qm.addInputs(parameter_dict)
-        qm.service()
-        return {}, 200
-
-    def post(self):
-        params = {}
-        params.update(request.form)
-
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'insertDetailGroupCode')
-        qm.addInputs(params)
-        qm.serviceUpdate()
-        results = qm.serviceResults()
-        return {}, 200
-# 아래는 샘플 예제
-# Todo = Namespace('Todo')
-# @Todo.route('')
-# class TodoPost(Resource):
-#     def post(self):
-#         global count
-#         global todos
-#
-#         idx = count
-#         count += 1
-#         todos[idx] = request.json.get('data')
-#
-#         return {
-#             'todo_id': idx,
-#             'data': todos[idx]
-#         }
-#
-#
-# @Todo.route('/<int:todo_id>')
-# class TodoSimple(Resource):
-#     def get(self, todo_id):
-#         return {
-#             'todo_id': todo_id,
-#             'data': todos[todo_id]
-#         }
-#
-#     def put(self, todo_id):
-#         todos[todo_id] = request.json.get('data')
-#         return {
-#             'todo_id': todo_id,
-#             'data': todos[todo_id]
-#         }
-#
-#     def delete(self, todo_id):
-#         del todos[todo_id]
-#         return {
-#             "delete": "success"
-#         }
