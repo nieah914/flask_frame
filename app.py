36f07f7fa0783272ee30a7857af9893c148456cd
정해인 <nieah914@naver.com>
2022-11-29 15:30:54 +0900

    edit 2

--- app.py
+++ app.py
@@ -1,26 +1,12 @@
-from flask import Flask, render_template , request
-import json
-import asyncio
-from flask_restx import Resource, Api
+from flask import Flask
+from flask_restx import Api
 from model.codeMaster import CodeMaster
-# from todo import Todo
-#
-# app = Flask(__name__)
-# api = Api(app)
-#
-# api.add_namespace(Todo, '/todos')
-# 기본은 아래
-# app = Flask(__name__)
 
 # static 추가
 app = Flask(__name__, static_folder='./static/')
 api = Api(app)
 
 api.add_namespace(CodeMaster, '/admin/codeMaster')
-
-
-
-
 
 if __name__=='__main__':
     app.run(debug=True,host='0.0.0.0',port='8080')
--- model/codeMaster.py
+++ model/codeMaster.py
@@ -1,22 +1,20 @@
 from flask import request
 from flask_restx import Resource, Api, Namespace
-from sql.sql_tools import QueryManager
 
 CodeMaster = Namespace(name='CodeMaster', description="코드마스터를 이용한 관리를 위한 부분")
 
 @CodeMaster.route('/groupCode')
 class CodeMasterGet(Resource):
     def get(self):
+        parameters = ''
+        if request.method == 'POST' or request.method == 'PUT' :
+            parameter_dict = request.form.to_dict()
+        else:
+            parameter_dict = request.args.to_dict()
+            for key in parameter_dict.keys():
+                parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
         print('/groupCode')
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'getGroupCode')
-        qm.addInputs([])
-        qm.service()
-
-        results = qm.serviceResults()
-        print(results)
-        # json_val = json.dumps(results)
-        return results
+        return "200"
 
     def put(self):
         parameters = ''
@@ -26,27 +24,10 @@ class CodeMasterGet(Resource):
             parameter_dict = request.args.to_dict()
             for key in parameter_dict.keys():
                 parameters += 'key: {}, value: {}\n'.format(key, request.args[key])
-            print(parameters)
-        print(parameter_dict)
 
-        print('111111')
-        if len(parameter_dict) == 0:
-            return 'No parameter'
-        print('111122')
-
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'updateGroupCode')
-        qm.addInputs(parameter_dict)
-        qm.service()
         return {}, 202
 
     def post(self):
         params = {}
         params.update(request.form)
-
-        qm = QueryManager()
-        qm.service_get_query('dashboard', 'sql_dashboard', 'insertGroupCode')
-        qm.addInputs(params)
-        qm.serviceUpdate()
-        results = qm.serviceResults()
         return {}, 200
