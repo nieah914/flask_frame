078c53eb45b7c5328a879f9ef75bcae785c7a083
정해인 <nieah914@naver.com>
2022-11-29 15:29:31 +0900

    edit

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
 from flask import Flask, render_template , request
-from sql.sql_tools import QueryManager
 import json
 import asyncio
 from flask_restx import Resource, Api
 from model.codeMaster import CodeMaster
-from model.main import Main
-from model.popup.mamul import POPUP_MAMUL
 # from todo import Todo
 #
 # app = Flask(__name__)
